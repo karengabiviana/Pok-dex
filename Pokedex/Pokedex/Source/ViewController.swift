e4541e7ff9cad72704458ebae7c66f6ae26f8b53
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2024-06-11 14:00:31 -0300

    Add textfield to main view

--- Pokedex/Pokedex/Source/ViewController.swift
+++ Pokedex/Pokedex/Source/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let textfield: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Que Pokémon você quer pesquisar?"
+        return field
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLayout()
     }
-
+    
+    func configureLayout() {
+        view.backgroundColor = .white
+        view.addSubview(textfield)
+        constrainstLayout()
+    }
+    
+    func constrainstLayout() {
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textfield.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
 
 }
 
