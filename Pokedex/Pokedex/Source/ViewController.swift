c34644844a362700625c4e006069706472c49c80
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2024-06-11 17:05:15 -0300

    Add done button

--- Pokedex/Pokedex/Source/ViewController.swift
+++ Pokedex/Pokedex/Source/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         field.borderStyle = .roundedRect
         return field
     }()
+    
+    let doneButton: UIButton = {
+        let action = UIAction(title: "Pesquisar", image: nil, identifier: nil) { _ in
+            print("Done button pressed")
+        }
+        let button = UIButton(configuration: .borderedTinted(), primaryAction: action)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +31,21 @@ class ViewController: UIViewController {
     func configureLayout() {
         view.backgroundColor = .white
         view.addSubview(textfield)
+        view.addSubview(doneButton)
         constrainstLayout()
     }
     
     func constrainstLayout() {
         textfield.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             textfield.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
             textfield.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            textfield.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40)
+            textfield.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            
+            doneButton.topAnchor.constraint(equalTo: textfield.bottomAnchor, constant: 20),
+            doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 
