52a921314b3eb5db94f87c60699377b0c758b088
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2024-06-27 17:51:39 -0300

    Add UIView extension Pin constraints

---
+++ Pokedex/Pokedex/Extensions/UIView+Pin.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Pin.swift
+//  Pokedex
+//
+//  Created by Karen Gabrieli Viana on 27/06/24.
+//
+
+import UIKit
+
+extension UIView {
+
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+    }
+}
